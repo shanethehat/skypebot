03a513c0d449d691806b9f963ab70b2eb710fc35
blongden <blongden@sessiondigital.com>
2012-05-24 08:54:56 -0500

    Some fixes

--- dbus-monitor.php
+++ dbus-monitor.php
@@ -6,8 +6,6 @@ $n = $d->createProxy( "com.Skype.API", "/com/Skype", "com.Skype.API");
 $n->Invoke( "NAME PHP" );
 $n->Invoke( "PROTOCOL 7" );
 
-SkypeBot::$n = $n;
-
 $d->registerObject( '/com/Skype/Client', 'com.Skype.API.Client', 'SkypeBot' );
 
 do {
--- skypebot.php
+++ skypebot.php
@@ -17,14 +17,14 @@ class SkypeBotEngine {
                 case 'BUDDYSTATUS':
                     if ($val == 3) {
                         // user accepted friend request - send welcome
-                        SkypeBot::notify(SkypeBot::$n->Invoke("CHAT CREATE $name,$name"));
+                        $this->parse($this->dbus->Invoke("CHAT CREATE $name,$name"));
                     }
                 break;
             }
         } else if($cmd === 'CHAT') {
             switch($arg) {
                 case 'NAME':
-                    SkypeBot::notify(SkypeBot::$n->Invoke("CHATMESSAGE $name Hi there! This is your new group chat channel. For github integration, add the URL http://incubator.inviqa.com:9001/github.php?id=".urlencode($name)." as a commit hook in your github repository."));
+                    $this->parse($this->dbus->Invoke("CHATMESSAGE $name Hi there! This is your new group chat channel. For github integration, add the URL http://incubator.inviqa.com:9001/github.php?id=".urlencode($name)." as a commit hook in your github repository."));
                 break;
             }
         } else {

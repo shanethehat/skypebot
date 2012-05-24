e30645cc02cfa68218dea1ed62a8c5825f14da31
Ben Longden <blongden@ibuildings.com>
2012-05-24 19:27:47 +0100

    Updated the welcome message.

--- skypebot.php
+++ skypebot.php
@@ -23,7 +23,7 @@ class SkypeBotEngine {
         } else if($cmd === 'CHAT') {
             switch($arg) {
                 case 'NAME':
-                    $this->parse($this->dbus->Invoke("CHATMESSAGE $name Hi there! This is your new group chat channel. For github integration, add the URL http://incubator.inviqa.com:9001/github.php?id=".urlencode($name)." as a commit hook in your github repository."));
+                    $this->parse($this->dbus->Invoke("CHATMESSAGE $name Hi there! This is your new group chat channel. Unfortunately I can't complete this whole process for you (though I would like to). For github integration, just add this URL; http://incubator.inviqa.com:9001/github.php?id=".urlencode($name)." as a commit hook in your github repository, and I will post all the commits that are pushed to it right here in this channel."));
                 break;
             }
         } else {

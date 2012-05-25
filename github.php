6538a47797f379443cb58ea10a1c9c691d91118a
Ben Longden <blongden@ibuildings.com>
2012-05-25 14:34:34 +0100

    Tweaked message

--- github.php
+++ github.php
@@ -2,23 +2,23 @@
 
 $message = array();
 if (isset($_REQUEST['payload'])) {
-	$payload = json_decode($_REQUEST['payload']);
-	
-	if ($payload) {
-		foreach ($payload->commits as $commit) {
-		    $lines = explode("\n", $commit->message);
-		    $message[] = '- ' . $lines[0] . '('.substr($commit->id, 0, 6).')';
-		}
+    $payload = json_decode($_REQUEST['payload']);
 
-		$info = sprintf(
-		    "%s pushed to %s at %s/%s.\n%s",
-		    $payload->head_commit->committer->name,
-		    $payload->ref,
-		    $payload->repository->organization,
-		    $payload->repository->name,
-		    join("\n", $message)
-		);
-	}
+    if ($payload) {
+        foreach ($payload->commits as $commit) {
+            $lines = explode("\n", $commit->message);
+            $message[] = '- ' . $lines[0] . ' ('.substr($commit->id, 0, 6).')';
+        }
+
+        $info = sprintf(
+            "%s pushed to %s at %s/%s.\n%s",
+            $payload->head_commit->committer->name,
+            $payload->ref,
+            $payload->repository->organization,
+            $payload->repository->name,
+            join("\n", $message)
+        );
+    }
 } else {
     $info = "I received an invalid request from someone (not GitHub?). If this continues, something is broken!";
 }

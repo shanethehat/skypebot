1f5fbb922ee3c4b612c73155efb576c35c7dd37a
Richard Miller <richard.miller@limethinking.co.uk>
2013-05-01 13:55:36 +0100

    Add badger command
    Posts link to a picture of Brian May in front of a picture of a badger
    Conflicts:
    
    	engine.php
    
    Conflicts:
    
    	engine.php

--- engine.php
+++ engine.php
@@ -20,4 +20,8 @@ $engine->add(':burritos?', function(SkypeEngine $engine, $chatname, $handle, $bo
     $engine->cmd("CHATMESSAGE {$chatname['val']} https://docs.google.com/a/inviqa.com/spreadsheet/ccc?key=0AgaDiKrNejnsdHRGTEFIMGxlOVVxQXpkbExYQlk1N2c");
 });
 
+$engine->add(':badger', function(SkypeEngine $engine, $chatname, $handle, $body) {
+    $engine->cmd("CHATMESSAGE {$chatname['val']} http://i.telegraph.co.uk/multimedia/archive/02414/badger_2414880b.jpg");
+});
+
 return $engine;

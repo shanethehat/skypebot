c4da5f98c8c6dfad8b512e74d4e8f15aaeb7178f
Tom Kitchin <tkitchin@inviqa.com>
2013-05-01 11:16:45 +0100

    Add key magento help functionality.

--- engine.php
+++ engine.php
@@ -12,4 +12,8 @@ $engine->add(':ping', function(SkypeEngine $engine, $chatname, $handle, $body) {
     $engine->cmd("CHATMESSAGE {$chatname['val']} Pong!");
 });
 
+$engine->add(':magento', function(SkypeEngine $engine, $chatname, $handle, $body) {
+    $engine->cmd("CHATMESSAGE {$chatname['val']} Have you tried clearing the cache?");
+});
+
 return $engine;

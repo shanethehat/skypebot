0a033d46094e290648e24273ed96ffb729bf5402
Shane Auckland <shane.auckland@gmail.com>
2013-05-01 12:05:49 +0100

    Add a question mark for Miller

--- engine.php
+++ engine.php
@@ -16,7 +16,7 @@ $engine->add(':magento', function(SkypeEngine $engine, $chatname, $handle, $body
     $engine->cmd("CHATMESSAGE {$chatname['val']} Have you tried clearing the cache?");
 });
 
-$engine->add(':burritos', function(SkypeEngine $engine, $chatname, $handle, $body) {
+$engine->add(':burritos?', function(SkypeEngine $engine, $chatname, $handle, $body) {
     $engine->cmd("CHATMESSAGE {$chatname['val']} https://docs.google.com/a/inviqa.com/spreadsheet/ccc?key=0AhCZJEVLpRf5dGN3VnhMWnBEZ1hDRjZ5TEFQc3JOclE");
 });
 
